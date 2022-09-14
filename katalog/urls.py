0ae180988242b33b9520442e875b76cc68903c5c
Ammar Ash shiddiq <ammar@Ammars-MacBook-Pro.local>
2022-09-14 21:48:46 +0700

    assignment-2-pbp-done

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import catalog_display
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('',catalog_display,name='show_catalog')
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def catalog_display(request):
+    data_item_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_item_catalog,
+        'name': 'Ammar Ash Shiddiq',
+        'npm': '2106656560'
+    }
+    return render(request, 'katalog.html', context)
